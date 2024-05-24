e14cbe04e3c055e179ab7ba4e5a61e67babbb80b
Saroj Tamang <sarojtmg992@gmail.com>
2024-05-24 10:34:10 +0100

    commit

--- logic.js
+++ logic.js
@@ -1,1 +1,21 @@
 const str = 'madam'
+
+//conv to array
+//use reverse
+//check if they are same or not
+
+function x(str){
+
+  const toArr = str.split('')
+
+  
+  const reverse = toArr.reverse()
+
+  
+  const join = reverse.join('')
+  
+str === join ? console.log(true) : console.log(false);
+  
+}
+
+x(str)
