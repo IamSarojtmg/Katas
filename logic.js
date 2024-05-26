974bd16a01f604b3840f0bd2d8311e41258e56f9
Saroj Tamang <sarojtmg992@gmail.com>
2024-05-26 09:01:57 +0100

    kata

--- logic.js
+++ logic.js
@@ -1,10 +1,10 @@
-let fic = 3
-let result = 1
+const largestNum = [1,2,3,44,5,6,77,100]
 
+const shallow = [...largestNum]
+let answer=0
+const result = shallow.filter(e=>{
+    // console.log(e);
+    answer = Math.max(e)
+})
 
-for (let i = 1; i <= fic; i++) {
-    // console.log(i);// use this to multiple
-    console.log(result*= i)
-}
-
-// console.log(result);
+console.log(answer);
