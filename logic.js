fcd57d6e8eb09060d29b538cdb0267294e01ec9a
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-01 17:03:16 +0100

    ts

--- logic.js
+++ logic.js
@@ -1,17 +1,1 @@
-function getData() {
-    console.log('Data Fetched')
-  }
-  
-  function debounce(callback, delay) {
-    let timer
-    return function() {
-    //   clearTimeout(timer) // built in method
-      timer = setTimeout(() => { //bult in method
-        callback(); 
-      }, delay)
-    }
-  }
-  
-  const betterFunction = debounce(getData, 1000)
-
-  betterFunction()
+console.log('ts');
---
+++ logic.ts
@@ -0,0 +1,9 @@
+let x = '1'
+let y = '2'
+
+function test(x:number, y:number) {
+    console.log(x+y);
+    
+}
+
+test(x,y)
