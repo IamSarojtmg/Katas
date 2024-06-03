b01ad37d62ec91852ed1567b4f1c82714856865f
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-03 10:58:37 +0100

    type annotation

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-;
-function getProduct(id) {
-    return {
-        id: id,
-        name: "Awesome Gadget ".concat(id),
-        price: 99.5
-    };
-}
-var showProduct = function (name, price) {
-    console.log("The product ".concat(name, " costs $").concat(price, "."));
+var concat = function (a, b) {
+    return a + b;
 };
+console.log(concat("hi", "world"));
+console.log(concat("3", "3"));
--- app.ts
+++ app.ts
@@ -1,10 +1,6 @@
-let id:number = 5;
-let company:string = 'lfc'
-let isPublished:boolean = true
+const concat = (a: string, b: string):boolean => {
+  return a+b;
+};
 
-
-
-
-let ids:number[] =[3,4,5]
-let plants:string[] =['fern','mexican fortune', 'snake plant']
-let x:any[] =['everest', 111]
+console.log(concat("hi", "world"));
+console.log(concat("3", "3"));
