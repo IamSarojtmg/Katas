7bce6b61c0ed83fe08bc2197031a45d1e5febe23
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-08 10:36:08 +0100

    zigzag

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 "use strict";
-const merc = {
-    name: "Mercedes",
-    COO: "Germany",
-    modelNum: 22,
-};
-merc.name = 'BMW';
-merc.modelNum = 99;
-console.log(merc);
+function solution(numbers) {
+    if (numbers.length < 3) {
+        return [];
+    }
+
+    let zigzags = [];
+    for (let i = 0; i < numbers.length - 2; i++) {
+        let a = numbers[i];
+        let b = numbers[i + 1];
+        let c = numbers[i + 2];
+        console.log(a,'a');
+        console.log(b,'b');
+
+        if ((a < b && b > c) || (a > b && b < c)) {
+            zigzags.push(1);
+        } else {
+            zigzags.push(0);
+        }
+    }
+
+    return zigzags;
+}
+
+// Example usage:
+const numbers = [1, 3, 2, 4, 1,];
+console.log(solution(numbers));  // Output: [1, 1, 1]
+
--- app.ts
+++ app.ts
@@ -1,17 +1,3 @@
-interface vehicle {
-  name: string;
-  COO: string;
-  readonly modelNum: number;
-}
-
-const merc: vehicle = {
-  name: "Mercedes",
-  COO: "Germany",
-  modelNum: 22,
-};
-
-merc.name = 'BMW'
-merc.modelNum = 99
-
-console.log(merc);
-
+function flipBooleans(bools) {
+  // Your code goes here...
+  }
