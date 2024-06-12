7d6322bce56082f7d2a500374e7e2a3d044c1c5b
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-12 14:07:13 +0100

    sorting

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-let nums1 = [1, 2, 3, 0, 0, 0];
-let nums2 = [2, 5, 6];
-let m = 3;
-let n = 3;
-
-const merge = (nums1, m, nums2, n) => {
-  let nums2Shallow = [...nums2]
-  let nums1Shallow = [...nums1];
-  nums1 = [];
-  nums1Shallow.filter((e) => {
-    if (!(e === 0)) {
-      nums1.push(e);
-    }
-  });
-  nums2Shallow.filter(e=>{
-    if (!(e===0)) {
-      nums1.push(e)
-    }
+// return the two oldest/oldest ages within the array of ages passed in.
+function twoOldestAges(ages) {
+  let sorting = ages.sort((a,b)=>{
+    return a-b
   })
-nums1.sort((a,b)=>{
-  return a-b
-})
+console.log(sorting);
 
-console.log(nums1);
-return nums1
+}
 
-};
+twoOldestAges([98,55,45,98]);
 
-merge(nums1, m, nums2, n);
 
-//input is array and array.length
-//desired output = need the combination of both array in ascending order without the 0
 
-//Breaking down the problem
-// get rid of the zeroes in the array.
-//sort it in asceding orger using sort
-//delete all the arrays inside both the nums 1 and 2
-// push the new array inside num 1
-
-//expected output num1[1,2,2,3,5,6], num2[]
