5df97942d9b6661e26368ddace683bb8c073c460
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-13 18:34:24 +0100

    remove after #

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-// return the two oldest/oldest ages within the array of ages passed in.
-function twoOldestAges(ages) {
-  let sorting = ages.sort((a,b)=>{
-    return a-b
-  })
-console.log(sorting);
+function removeUrlAnchor(url) {
+// console.log(url.length);
+let index = url.indexOf('#')
+// console.log(index, 'index');
+
+let sub = url.length - index
+
+// console.log(sub,'sub');
+
+
+
+
+
+  if(url.includes('#')){
+// return url.slice(9,-sub)
+   console.log(url.slice(0,-sub))
+ }
+
+ return url
+
 
 }
-
-twoOldestAges([98,55,45,98]);
-
-
-
+removeUrlAnchor("www.codewars.com#about");
+removeUrlAnchor("https://toast.mil#broc");
+removeUrlAnchor("www.codewars.com/katas/");
+removeUrlAnchor('http://www.egg.com#p')
+removeUrlAnchor('www.egg.gov#mieng')
