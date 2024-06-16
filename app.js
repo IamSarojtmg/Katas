ac0cb527f48dd534394651cc5f84d9ce763918a7
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-16 11:48:30 +0100

    find index

--- app.js
+++ app.js
@@ -1,18 +1,61 @@
-var isAnagram = function (test, original) {
-  let result;
-  const x = test.toLowerCase().split("").sort();
-  const y = original.toLowerCase().split("").sort();
-  for (let i = 0; i < x.length; i++) {
-    if (!(x[i] === y[i])) {
-      result = false;
-      break;
-    } else {
-      result = true;
+// function spot(s1, s2) {
+//   let result = [];
+//   // console.log(s1===s2);
+//   if (s1 === s2) {
+//     result = []
+//   }
+
+//   //if both str are same or is an empty array reutrn []
+//   //if s1 and s2 are not same return indexOf s1[i]
+
+//   //problem - if there is another similar index than its returning that one. 'abcda','abcde' return index of 0 because the for loop is returning the first index rather than the original one.
+
+//   for (let i = 0; i < s1.length; i++) {
+//     // console.log(s1[i],'S1',s1.indexOf(s1[i], s1[i]===s2[i]));
+//     // console.log(s2[i],'S2', s2.indexOf(s2[i]),s1[i]===s2[i]);
+//     if (s1[i] !== s2[i]) {
+//       // console.log(s1[i],'S1');
+//       console.log(s1.length);
+//       result.push(s1.indexOf(s1[i]));
+//     }
+//   }
+
+//   console.log(result);
+//   return result
+// }
+
+
+
+
+
+
+function spot(s1, s2) {
+  let s1Split = s1.split("");
+  let s2Split = s2.split("");
+  let arr = [];
+  if (s1 === s2) {
+    arr = [];
+  }
+
+  for (let i = 0; i < s1Split.length; i++) {
+    const element = s1Split[i];
+    if (element !== s2Split[i]) {
+      console.log(i);
+      arr.push(i)
     }
   }
 
-  console.log(result);
-  return result;
-};
+  console.log(arr);
+  return arr;
+}
 
 
+
+spot('abcda', 'abcde') //4
+// spot(
+//   "hC.bSCjaaDmH.HHHJUa!utpGf.TzNdnllQUbzKii ",
+//   "hC. SCjaaDVH.HHHJUs! tpGf.xzNdnlzhUbzKii "
+// );
+// spot("abcdefg", "abcqetg");
+// spot("FixedGrey", "FixedGrey");
+// spot("Hello World!", "hello world.");
