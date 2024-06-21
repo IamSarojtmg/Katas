a80c6c66d8b4a8fccc1be8dded286efe9929038d
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-21 10:32:56 +0100

    Stop gninnipS My sdroW

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-function solution(number) {
-  if (number <= 0) {
-    return 0;
+function spinWords(string) {
+  if (string.split(" ").length <= 1) {
+    return string.split("").reverse().join("");
   }
-  let newArr = [];
-  let sum = 0;
-
-  for (let i = 1; i < number; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      newArr.push(i);
-    }
-  }
-
-  newArr.map((e) => {
-    sum += e;
+  let arr = [];
+  string.split(" ").map((e) => {
+    if (e.length >= 5) {
+      arr.push(e.split("").reverse().join(""));
+    } else arr.push(e);
   });
-  return sum;
+
+  return arr.join(" ");
 }
+
+spinWords("Welcome");
+spinWords("Hey fellow warriors");
