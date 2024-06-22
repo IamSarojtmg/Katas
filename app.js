4a98f7f063ff99fc1c3556805d27ae4f27431e3f
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-22 11:33:14 +0100

    Who likes it?

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-function spinWords(string) {
-  if (string.split(" ").length <= 1) {
-    return string.split("").reverse().join("");
-  }
-  let arr = [];
-  string.split(" ").map((e) => {
-    if (e.length >= 5) {
-      arr.push(e.split("").reverse().join(""));
-    } else arr.push(e);
-  });
+function likes(names) {
+  let result = "";
+  if (names.length === 0) {
+    result = "no one likes this";
+  } else if (names.length === 1) {
+    result = names[0] + " likes this";
+  } else if (names.length > 1 && names.length < 4) {
+    const lastName = " and " + names[names.length - 1] + " like this";
+    names.pop();
+    result = names.join(", ") + lastName
+  } else {
+    const firstTwo = `${names[0]}, ${names[1]}`;
+    for (let i = 0; i < 2; i++) {
+      names.shift();
+    }
 
-  return arr.join(" ");
+    result = `${firstTwo} and ${names.length} others like this`;
+  }
+console.log(result);
+  return result;
 }
 
-spinWords("Welcome");
-spinWords("Hey fellow warriors");
+likes([]);
+likes(["Peter"]);
+likes(["Jacob", "Alex"]); //'Jacob and Alex like this'
+likes(["Max", "John", "Mark"]); //'Max, John and Mark like this'
+likes(["Alex", "Jacob", "Mark", "Max"]); //'Alex, Jacob and 2 others like this'
