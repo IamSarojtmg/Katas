9cb1d54cb33e7f8c0b09c0d7c29da5061b0c7519
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-28 19:51:59 +0100

    trying reduce

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ function partsSums(ls) {
   //add the total of the element in ls and send to a new array
   //git rid of the first element in ls
   //than loop it again until all of the elements are deleted.
+
+  const sum1 = ls.reduce((p,c)=>{
+    console.log( p);
+    console.log( c);
+    console.log(ls);
+    ls.shift()
+    console.log(ls);
+    console.log(ls.length);
+return p+c
+
+  },0)
+  // console.log(sum1);
   let sum = 0;
   const newArr = [];
 
   for (let i = 0; i < ls.length; i++) {
-    if (ls.length > 0) {
+    if (ls.length > 0 ) {
       const element = ls[i];
       sum += element;
     }
@@ -14,7 +26,7 @@ function partsSums(ls) {
   newArr.push(sum);
   sum = 0;
   ls.shift();
-  console.log(newArr); //6 
+
 }
 
 partsSums([1, 2, 3]); //answer [6,5,3,0]
