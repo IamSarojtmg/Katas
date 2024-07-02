d7fcc59b85c273d7dcdb8ca7de10c28d0b60a248
Saroj Tamang <sarojtmg992@gmail.com>
2024-07-02 08:39:06 +0100

    bracket notation

--- app.js
+++ app.js
@@ -1,37 +1,29 @@
-function partsSums(ls) {
-  //add the total of the element in ls and send to a new array
-  //git rid of the first element in ls
-  //than loop it again until all of the elements are deleted.
-
-  const sum1 = ls.reduce((p,c)=>{
-    console.log( p);
-    console.log( c);
-    console.log(ls);
-    ls.shift()
-    console.log(ls);
-    console.log(ls.length);
-return p+c
-
-  },0)
-  // console.log(sum1);
-  let sum = 0;
-  const newArr = [];
-
-  for (let i = 0; i < ls.length; i++) {
-    if (ls.length > 0 ) {
-      const element = ls[i];
-      sum += element;
-    }
-  }
-  newArr.push(sum);
-  sum = 0;
-  ls.shift();
-
+function findOdd(A) {
+  //check how many time each number appears.
+  let count = {}
+  const x = A.forEach(e => {
+// console.log(count);
+if(count[e]){
+  count[e]+=1
+}else{
+  count[e] = 1
+}
+  });
+  
+  console.log(count);
+  return count;
 }
 
-partsSums([1, 2, 3]); //answer [6,5,3,0]
+findOdd([1,1,2])
 
-//Loop || javascript
-//Been stuck on this for a while.
-//I cannot seem to find a way to run the loop again.
-//Any advise?
+
+
+
+
+
+
+
+
+
+// return than number
+// if that number is not divisible by 2 than return that number in the array.
