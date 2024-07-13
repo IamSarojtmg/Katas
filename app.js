f69eb3df0706c2d87cf95a47d85ca5ef2f84b85d
Saroj Tamang <sarojtmg992@gmail.com>
2024-07-13 11:46:42 +0100

    east north west south

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-function duplicateCount(text) {
-  let newObj = {};
-  let arr = []
-  for (let i = 0; i < text.toLowerCase().length; i++) {
-    if (newObj[text[i]]) {
-      newObj[text[i]] += 1;
-    } else {
-      newObj[text[i]] = 1;
-    }
-  }
+function isValidWalk(walk) {
+  let result
+if(walk.length !== 10) return false
 
-  for(const obj in newObj){
-    if(newObj[obj]>1){
-      arr.push(obj)
-    }
+let n = 0
+let s = 0
+let east = 0
+let w = 0
+
+walk.forEach(e=>{
+  console.log(e);
+  if (e==='n'){
+    n++
+  }else if(e==='s'){
+    s++
+  } else if (e === 'e'){
+    east++
+  }else{
+    w++
   }
-console.log(arr.length);
+})
+if(n===s && east === w ){
+  result = true
+}else{
+  result = false
+}
+console.log(result);
+return result
 }
 
-duplicateCount("aabBcde");
+
+// isValidWalk(['n','s','n','s','n','s','n','s','n','s'])
+// isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e'])
+// isValidWalk(['w','w'])
+// isValidWalk(['n','n','n','s','n','s','n','s','n','s'])
+isValidWalk(['n','n','e','s','s','s','s','w','n','n'])
