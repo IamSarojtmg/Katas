5aea11fa5cb8fbafd76ba222fffd1b0a96438e20
Saroj Tamang <sarojtmg992@gmail.com>
2024-07-22 11:07:12 +0100

    alphabets

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var position = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var newArr = [];
 function alphabetPosition(text) {
+    var position = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var newArr = [];
     for (var i = 0; i < text.length; i++) {
         var element = text[i].toLowerCase();
-        // console.log(element);
-        // console.log(position.indexOf(element))
         newArr.push(position.indexOf(element) + 1);
     }
-    console.log(newArr);
+    var filter = newArr.filter(function (e) {
+        return e !== 0;
+    });
+    return filter.toString().replaceAll(',', ' ');
 }
-// alphabetPosition("The sunset sets at twelve o' clock.")//"20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
-alphabetPosition("The narwhal bacons at midnight."); //20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20
+alphabetPosition("The sunset sets at twelve o' clock."); //"20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+
--- app.ts
+++ app.ts
@@ -1,19 +1,23 @@
-const position:string[] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
-const newArr:number[] = []
-
-function alphabetPosition(text:string) {
+function alphabetPosition(text:string):string {
+  const position:string[] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const newArr:number[] = []
 for (let i = 0; i < text.length; i++) {
   const element = text[i].toLowerCase()
-  // console.log(element);
-  
-  // console.log(position.indexOf(element))
   newArr.push(position.indexOf(element)+1)
-  
+  }
+const filter = newArr.filter(e=>{
+  return e !== 0
+  })
+return filter.toString().replaceAll(',',' ')
 }
-console.log(newArr);
+alphabetPosition("The sunset sets at twelve o' clock.")//"20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
 
-}
 
-// alphabetPosition("The sunset sets at twelve o' clock.")//"20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
-alphabetPosition("The narwhal bacons at midnight.") //20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20
+
+
+// let x:string = ''
+// let y:number[] = [1,2,3]
+
+// x = y.toString()
+
+// console.log(x.replaceAll(',',' '));
