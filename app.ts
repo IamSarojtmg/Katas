402ac52a7572d771c080226415ca20ce1f396368
Saroj Tamang <sarojtmg992@gmail.com>
2024-08-01 11:22:14 +0100

    .

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ for (let i = 0; i < text.length; i++) {
 const filter = newArr.filter(e=>{
   return e !== 0
   })
-return filter.toString().replaceAll(',',' ')
+return filter.toString().replaceAll(',',' ')//
 }
 alphabetPosition("The sunset sets at twelve o' clock.")//"20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
 
