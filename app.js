a46db1acb6961f42276bc1b660d19449793d5f9f
Saroj Tamang <sarojtmg992@gmail.com>
2024-10-18 11:51:52 +0100

    replace

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-function checkThreeAndTwo(array) {
-  // console.log(array);
-  //find dub
-  //send to an array
-  //length of three arr is 3 and length of 2 arr is 2 return true else false
-  const x = array.filter((e, i) => {
-    return array.indexOf(e) !== i;
-  });
-  console.log(x);
-  
-}
-
-checkThreeAndTwo(["a", "a", "a", "b", "b"]);
+function disemvowel(str) {
+    return str.replace(/[aeiou]/gi, '');
+  }
