73c44bb3c51cbd6c8243f87ba21bbd069898a81e
Saroj Tamang <sarojtmg992@gmail.com>
2024-10-22 11:30:31 +0100

    square

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-function disemvowel(str) {
-    return str.replace(/[aeiou]/gi, '');
+function squareDigits(num) {
+  const shallow = num.toString();
+  const newArr = [];
+  for (let i = 0; i < shallow.length; i++) {
+    const element = shallow[i];
+    newArr.push(element * element);
   }
+
+  return Number(newArr.join(""));
+}
+
+squareDigits(3212), 9414;
