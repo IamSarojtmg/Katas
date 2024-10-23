7a1367ac92878e45d3ffbf6734440c54c774104f
Saroj Tamang <sarojtmg992@gmail.com>
2024-10-23 10:47:13 +0100

    reduce

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-function squareDigits(num) {
-  const shallow = num.toString();
-  const newArr = [];
-  for (let i = 0; i < shallow.length; i++) {
-    const element = shallow[i];
-    newArr.push(element * element);
-  }
+function persistence(num) {
+const shallow = num.toString().split('').map(Number)
+let newArr = [];
+const secondArr = []
 
-  return Number(newArr.join(""));
+if(shallow.length>1){
+for (let i = 0; i < shallow.length; i++) {
+  const element = shallow[i];
+  let times = element * shallow[i+1]
+  newArr.push(times)
+}
+if(newArr[0].toString().split('').map(Number).length > 1){
+  for (let i = 0; i < newArr[0].toString().split('').map(Number).length; i++) {
+    const element = shallow[i];
+    let times = element * shallow[i+1]
+    newArr = []
+    newArr.push(times)
+  }
+}
+console.log(newArr);
+
 }
 
-squareDigits(3212), 9414;
+//if length is more than 1 seperate the digits [3,9] if not return the number
+//times all of the element
+// send the result into new array
+//continue until its single length
+
+
+}
+
+persistence(39),3
